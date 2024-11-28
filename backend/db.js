a519470f9654dd54c9b11c05e63b4e7046268ace
cris <c.arbizupombo2004@gmail.com>
2024-11-28 10:53:50 +0100

    Cambio hola Cris

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,10 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'victor25',
+    password: '',
     database: 'CRMsvelte',
 });
 
 module.exports = db;
+
+//HOLA
