71fabbdacec69c757098351589b6e8b1945cdf5e
Juanma <jmconsentino@gmail.com>
2024-11-28 10:48:33 +0100

    Cambio SQL Juanma

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'victor25',
+    //password: 'victor25',
+    password: '',
     database: 'CRMsvelte',
 });
 
