6587b92f0ea6c4352cbfa9e0e276ddafa48efbdb
Victor <victorlopezlopez93@gmail.com>
2024-12-05 09:55:56 +0100

    borrada password

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,6 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'victor25',
-    //password: '',
     database: 'crmsvelte',
 });
 
